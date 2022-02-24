5376047b12b08311b86e5e2b71021b5d40d184e9
monirhosenakanda <monirhosenakanda@gmail.com>
2022-02-25 00:27:21 +0600

    premitive and non premitive

---
+++ Primitive.js
@@ -0,0 +1,25 @@
+// data type list 
+// primavtive 
+// 1. number
+// 2. String 
+// 3. boolean 
+// 4. undefined 
+// 5. null 
+ 
+// 7. symbol 
+// non primative 
+// 6 . object
+
+
+let a = 'hello';
+let b = a;
+console.log(a,b);
+a = 'gello';
+console.log(a,b);
+
+
+const x = {job:'web Developer'}
+const y = x;
+console.log(x,y);
+x.job = 'font end developer';
+console.log(x,y);
---
+++ data-type.js
@@ -0,0 +1,6 @@
+// let a  = 19;
+
+let a = 'jamal uddin shahen shah';
+console.log(typeof a);
+
+//javascript is dynamic data type
